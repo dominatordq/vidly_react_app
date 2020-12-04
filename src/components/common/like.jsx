5fbf7f9b0a18482f54cc314f64ad1891c0e75f84
dominatordq <dominatordq@gmail.com>
2020-12-04 13:11:54 -0700

    Added some components (like, pagination) and services (fake database)

---
+++ src/components/common/like.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// Responsible for rendering an empty or full heart
+// Input: liked: boolean
+// Output: onClick
+
+// Stateless Functional Component
+const Like = (props) => {
+    let classes = "fa fa-heart";
+    if(!props.liked) classes += "-o"
+    
+    return ( <i 
+        onClick={props.onClick} 
+        style={{ cursor: "pointer" }} 
+        className={classes} 
+        aria-hidden="true"
+        /> );
+}
+ 
+export default Like;
